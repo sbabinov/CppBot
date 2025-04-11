928d077e7dbb8d177dc3d836f22546dd8413c759
Serafim Babinov <serafimbabinov106@gmail.com>
2025-04-11 20:29:01 +0300

    feat: full realization of state machine

--- src/state.hpp
+++ src/state.hpp
@@ -4,30 +4,56 @@
 #include <unordered_map>
 #include <boost/any.hpp>
 
-namespace state
+namespace states
 {
-  class Data
+  class State
   {};
 
   class StatesForm
   {
-    Data data;
+   public:
+    using Data = std::unordered_map< State, boost::any >;
   };
 
-  class State
+  class Storage
   {
+   public:
+    friend class StateMachine;
+    friend class StateContext;
+
+    Storage();
+    StatesForm::Data getData(size_t chatId);
+    boost::any getData(size_t chatId, const State& state);
+    void remove(size_t chatId);
+   private:
+    std::unordered_map< size_t, State > currentStates_;
+    std::unordered_map< size_t, StatesForm::Data > data_;
+  };
+
+  class StateMachine
+  {
+   public:
+    friend class StateContext;
+
+    StateMachine(Storage* storage);
+    void setState(size_t chatId, const State& state);
+    State getState(size_t chatId);
+   private:
+    static State defaultState_;
+    Storage* storage_;
   };
 
   class StateContext
   {
    public:
-    Data data;
-    StateContext(size_t chatId):
-      chatId_(chatId)
-    {}
+    StateContext(size_t chatId, StateMachine* stateMachine);
     void setState(const State& state);
+    void resetState();
+    StatesForm::Data getData();
+    boost::any getData(const State& state);
    private:
     size_t chatId_;
+    StateMachine* stateMachine_;
   };
 }
 
---
+++ src/states.cpp
@@ -0,0 +1,59 @@
+#include "state.hpp"
+
+states::StateMachine::StateMachine(Storage* storage):
+  storage_(storage)
+{}
+
+states::State states::StateMachine::getState(size_t chatId)
+{
+  return storage_->currentStates_[chatId];
+}
+
+void states::StateMachine::setState(size_t chatId, const State& state)
+{
+  storage_->currentStates_[chatId] = state;
+}
+
+states::Storage::Storage():
+  data_()
+{}
+
+boost::any states::Storage::getData(size_t chatId, const states::State& state)
+{
+  return data_[chatId][state];
+}
+
+states::StatesForm::Data states::Storage::getData(size_t chatId)
+{
+  return data_[chatId];
+}
+
+void states::Storage::remove(size_t chatId)
+{
+  data_[chatId].clear();
+}
+
+states::StateContext::StateContext(size_t chatId, states::StateMachine* stateMachine):
+  chatId_(chatId),
+  stateMachine_(stateMachine)
+{}
+
+void states::StateContext::setState(const states::State& state)
+{
+  stateMachine_->storage_->currentStates_[chatId_] = state;
+}
+
+void states::StateContext::resetState()
+{
+  stateMachine_->storage_->currentStates_[chatId_] = stateMachine_->defaultState_;
+}
+
+states::StatesForm::Data states::StateContext::getData()
+{
+  return stateMachine_->storage_->getData(chatId_);
+}
+
+boost::any states::StateContext::getData(const states::State& state)
+{
+  return stateMachine_->storage_->getData(chatId_, state);
+}
