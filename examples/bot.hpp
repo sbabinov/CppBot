58df109831a43791b32294823401fccb44457447
Serafim Babinov <serafimbabinov106@gmail.com>
2025-04-19 19:18:58 +0300

    fix: remove bot token

--- examples/bot.hpp
+++ examples/bot.hpp
@@ -11,7 +11,7 @@ namespace app
   std::shared_ptr< handlers::MessageHandler > messageHandler = std::make_shared< handlers::MessageHandler >();
   std::shared_ptr< handlers::CallbackQueryHandler > queryHandler = std::make_shared< handlers::CallbackQueryHandler>();
   std::shared_ptr< states::Storage > storage = std::make_shared< states::Storage >();
-  cppbot::Bot bot("7864465597:AAEJEeW2JfyQY01q-ii111s4kRbMzj4y5dI", messageHandler, queryHandler, storage);
+  cppbot::Bot bot("YOUR_TOKEN_HERE", messageHandler, queryHandler, storage);
 }
 
 #endif
